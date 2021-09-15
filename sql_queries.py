719b3500016278563e62f8a19372ec2e58e2843c
phuquyng <nguyenphuquy2303@gmail.com>
2021-09-15 20:33:43 +0700

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -1,8 +1,11 @@
 # DROP TABLES
-
+songplays_table_drop = "DROP TABLE IF EXISTS songplays"
+users_table_drop = "DROP TABLE IF EXISTS users"
+songs_table_drop = "DROP TABLE IF EXISTS songs"
+artists_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-
 # song_id and artist_id haven't constraints NOT NULL because the sample data to test ETL doesn't have all the
 # combinations and most are null in both cases.
 songplay_table_create = 
@@ -25,7 +28,6 @@ song_table_insert =
 
 artist_table_insert = 
 
-
 time_table_insert = 
 
 # FIND SONGS
@@ -36,5 +38,5 @@ song_select =
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create,
                         artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop,
+drop_table_queries = [songplays_table_drop, users_table_drop, songs_table_drop, artists_table_drop,
                       time_table_drop]
