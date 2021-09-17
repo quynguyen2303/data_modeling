184bb8e97039b3bf975658a675efd496fc4dc623
phuquyng <nguyenphuquy2303@gmail.com>
2021-09-17 21:19:00 +0700

    update elt first half

--- sql_queries.py
+++ sql_queries.py
@@ -61,19 +61,44 @@ CREATE TABLE IF NOT EXISTS time
 
 # INSERT RECORDS
 
-# songplay_table_insert = 
+songplay_table_insert = ("""
+INSERT INTO songlays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+""")
 
-# user_table_insert = 
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+        DO NOTHING
+""")
 
-# song_table_insert = 
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id)
+        DO NOTHING
+""")
 
-# artist_table_insert = 
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id)
+        DO NOTHING
+""")
 
-# time_table_insert = 
+time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time)
+        DO NOTHING
+""")
 
 # FIND SONGS
 
-# song_select = 
+song_select = ("""
+SELECT 
+""")
 
 # QUERY LISTS
 
